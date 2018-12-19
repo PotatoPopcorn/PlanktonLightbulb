1d82787e56ea3c656d1223d4510ae9db233a7069
Tony Kerr <potatopopcorn@potatopopcorn.com>
2018-12-19 23:47:24 +1000

    Implement Artnet from LPL

--- settingsartnet.cpp
+++ settingsartnet.cpp
@@ -12,3 +12,75 @@ SettingsArtnet::~SettingsArtnet()
 {
     delete ui;
 }
+
+bool SettingsArtnet::sendUni(PlanktonLighting::PLUniverse *uni)
+{
+    if(isRunning)
+    {
+        return device->sendDMX(uni);
+    }
+    return false;
+}
+
+bool SettingsArtnet::stopDevice()
+{
+    if(isRunning)
+    {
+        if(device->closeDevice())
+        {
+            ui->addressEdit->setEnabled(true);
+            ui->portEdit->setEnabled(true);
+            ui->physicalEdit->setEnabled(true);
+            ui->subNetEdit->setEnabled(true);
+            ui->netEdit->setEnabled(true);
+            ui->sequenceBox->setEnabled(true);
+            ui->startButton->setEnabled(true);
+            ui->stopButton->setEnabled(false);
+            isRunning = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+void SettingsArtnet::on_startButton_clicked()
+{
+    device = new PlanktonLighting::PLDeviceArtnet();
+    QString args;
+    args.append(ui->addressEdit->text());
+    args.append(" ");
+    args.append(ui->portEdit->text());
+    args.append(" ");
+    args.append(ui->physicalEdit->text());
+    args.append(" ");
+    args.append(ui->subNetEdit->text());
+    args.append(" ");
+    args.append(ui->netEdit->text());
+    args.append(" ");
+    if(ui->sequenceBox->currentText() == "On")
+    {
+        args.append("1");
+    }
+    else
+    {
+        args.append("0");
+    }
+    if(device->initDevice(args.toStdString()))
+    {
+        ui->addressEdit->setEnabled(false);
+        ui->portEdit->setEnabled(false);
+        ui->physicalEdit->setEnabled(false);
+        ui->subNetEdit->setEnabled(false);
+        ui->netEdit->setEnabled(false);
+        ui->sequenceBox->setEnabled(false);
+        ui->startButton->setEnabled(false);
+        ui->stopButton->setEnabled(true);
+        isRunning = true;
+        qDebug() << "Running";
+    }
+}
+
+void SettingsArtnet::on_stopButton_clicked()
+{
+    stopDevice();
+}
--- settingsartnet.h
+++ settingsartnet.h
@@ -1,7 +1,11 @@
 #ifndef SETTINGSARTNET_H
 #define SETTINGSARTNET_H
 
+#include <QDebug>
 #include <QWidget>
+
+#include "pldeviceartnet.h"
+#include "pluniverse.h"
 
 namespace Ui {
 class SettingsArtnet;
@@ -15,8 +19,19 @@ public:
     explicit SettingsArtnet(QWidget *parent = nullptr);
     ~SettingsArtnet();
 
+    bool sendUni(PlanktonLighting::PLUniverse *uni);
+    bool stopDevice();
+
+private slots:
+    void on_startButton_clicked();
+
+    void on_stopButton_clicked();
+
 private:
+    bool isRunning = false;
     Ui::SettingsArtnet *ui;
+
+    PlanktonLighting::PLDeviceArtnet *device;
 };
 
 #endif // SETTINGSARTNET_H
